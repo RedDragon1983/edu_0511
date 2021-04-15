a6c61a36e1cfca662a016915089c733f059850b8
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-15 19:50:29 +0300

    Update Main.java

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -11,6 +11,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        //Ваш код
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        System.out.println(a * b * c);
     }
 }
