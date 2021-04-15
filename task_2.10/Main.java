2fc168c5daee406d0cf7671617968863e6210aaf
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-15 19:53:23 +0300

    Update Main.java

--- task_2.10/Main.java
+++ task_2.10/Main.java
@@ -5,6 +5,16 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+        int t = 0;
+        for(int i = 0; i < (args.length-1); i++){
+            if (args[i] == args[i+1]) {
+                t = 1;
+            }
+        }
+        if(t == 1){
+            System.out.println("да");
+        }else {
+            System.out.println("нет");
+        }
     }
 }
