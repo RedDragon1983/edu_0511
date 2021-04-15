8885ad6d35818405bd05493d98738da437fe4dfb
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-15 20:54:37 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -4,7 +4,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner s = new Scanner(System.in);
+        Double sum = s.nextDouble();
+        Double p = s.nextDouble();
+        Double proc = sum/100;
+        Double i = sum;
+        for(int h = 0; h < 5; h++){
+            i+= sum + (proc*p);
+            System.out.println(i);
+        }
     }
 }
