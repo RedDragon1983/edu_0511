c5ecf69d9aadbd7ece819565d48c80d86009890d
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-15 21:08:36 +0300

    Update Main.java

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -5,6 +5,8 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println((a%b == 0) ? "Делится" : "Делится с остатком");
     }
 }
