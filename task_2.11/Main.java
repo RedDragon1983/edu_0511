c9faf52b267ca93dceb85092763dc0e8a55cccbc
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-15 19:54:28 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -4,7 +4,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int t = 1;
+        int t2 = 0;
+        for(int i = 0; i < args.length; i++){
+            t++;
+            t2+=Integer.parseInt(args[i]);
+            if (t2 > 10) {
+                System.out.println(t);
+                break;
+            }
+        }
 
     }
 }
