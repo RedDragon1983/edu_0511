54626dba500edf5d90ee9c3505ad31e155e42808
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-15 19:46:17 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -4,6 +4,7 @@
 public class Main {
     public static void main(String[] args) {
         int number = 25;
-        //напишите тут ваш код
+        System.out.println(number*2);
+        
     }
 }
