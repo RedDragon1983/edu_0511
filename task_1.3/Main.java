f0e21188201da7f291166a1d9c6d62fce6444431
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-15 19:45:14 +0300

    Update Main.java

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,8 +1,10 @@
+  
 /* Объяви переменные a, b типа int и переменную s типа String.
 Сразу же в строке объявления присвой им любые разные значения. */
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int a, b;
+        String s = new String();
     }
 }
