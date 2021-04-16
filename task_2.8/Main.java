bc846dc881010189e30907e2e259f11d6b7062c4
RedDragon1983 <82583556+RedDragon1983@users.noreply.github.com>
2021-04-16 15:24:52 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -5,6 +5,11 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+        int[] ints = {2, 3, 4, 5};
+        int composition = 1;
+        for (int i = 0; i < ints.length; i++){
+            composition*=ints[i];
+        }
+        System.out.println(composition);
     }
 }
