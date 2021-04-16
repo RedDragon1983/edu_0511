bd56c2657b9507f71ba9b11b2bde9791d834d086
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-16 23:29:56 +0300

    Update Main.java

--- task_3.5/Main.java
+++ task_3.5/Main.java
@@ -5,6 +5,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        int num[] = {4,2,6,2,65}
+        int num[] = {4,2,6,2,65};
+        all(0, num);
+    }
+    public static void all(int n, int[] ar){
+        System.out.println(ar[n]);
+        if(n < (ar.length-1)) {
+            all(n+1, ar);
+        }
     }
 }
