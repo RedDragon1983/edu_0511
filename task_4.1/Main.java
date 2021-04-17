53223758f1f48243d86abd0671eab13a8756bdb1
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-17 18:48:50 +0300

    Update Main.java

--- task_4.1/Main.java
+++ task_4.1/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.setName("Жужик");
-        System.out.println(cat.name);
+        //System.out.println(cat.name); // НЕТ ДОСТУПА К ПЕРЕМЕННОЙ БЕЗ ГЕТТЕРА
     }
 }
 
