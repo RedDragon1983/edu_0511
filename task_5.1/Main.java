b6724ada91b571973ef94cc24dc03ef1e9262091
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-19 17:07:37 +0300

    Update Main.java

--- task_5.1/Main.java
+++ task_5.1/Main.java
@@ -11,11 +11,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> names = new ArrayList();
+        ArrayList<String> test = new ArrayList();
         names.add("Анатолий");
         names.add("Георгий");
         names.add("Руслан");
         names.add("Георгий");
         names.add("Павел");
         names.add("Руслан");
+        
+        Iterator iterator = names.iterator();
+        Object t = "";
+        while (iterator.hasNext()) {
+            t = iterator.next();
+            if(test.contains(t)){
+                iterator.remove();
+            }else {
+               test.add(t.toString());
+            }
+        }
+        System.out.println(names);
     }
 }
