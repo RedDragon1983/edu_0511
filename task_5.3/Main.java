9210fc986eac3f1f052ecbe5b6fe99d0535bff44
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-20 07:29:38 +0300

    Update Main.java

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -4,8 +4,20 @@
 */
 
 import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-    
+        ArrayList<Integer> allNumbs = new  ArrayList<>();
+        ArrayList<Integer> total = new  ArrayList<>(); // новая коллекция
+        Random r = new Random();
+        for (int i = 0; i < 50_000; i++){
+            allNumbs.add(r.nextInt(50));
+        }
+        for (Integer j : allNumbs){
+            if(j > 0 && j < 10)
+                total.add(j);
+        }
+        System.out.println(total);
     }
 }
