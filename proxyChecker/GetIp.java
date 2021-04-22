d157de03460846e8f12956780cf9f4fbde7b551f
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-22 17:12:59 +0300

    Update GetIp.java

--- proxyChecker/GetIp.java
+++ proxyChecker/GetIp.java
@@ -1,1 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class GetIp {
+    public static void main(String[] args) {
+        try {
+            URL uri = new URL("https://vozhzhaev.ru/test.php");
+            BufferedReader br = new BufferedReader(new InputStreamReader(uri.openStream()));
+            while (br.ready())
+                System.out.println(br.readLine());
+        } catch (MalformedURLException e) {
+
+        } catch (IOException e) {
+
+        }
+    }
+}
