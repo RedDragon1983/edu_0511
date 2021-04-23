e487f1081e726757c96592ddb47cd4c5ce9e54c7
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-23 21:25:04 +0300

    Update Main.java

--- proxyChecker/Main.java
+++ proxyChecker/Main.java
@@ -23,21 +23,35 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            FileInputStream fin = new FileInputStream("C:\\java\\ip.txt");
+            //FileInputStream fin = new FileInputStream("C:\\java\\ip.txt");
+            FileInputStream fin = new FileInputStream("C:\\edu_0511_java\\ip.txt");
+
+            ArrayList<CheckProxy> arrayList = new ArrayList<>();
+            ArrayList<Thread> arrayList2 = new ArrayList<>();
+
             int i;
             String proxy = "";
             while ((i=fin.read()) != -1){
                 if(i==13) continue;
                 else if(i==10){
-                    String ip = proxy.split(":")[0];
-                    String port=proxy.split(":")[1];
-                    Thread thread = new Thread(new Runnable() {
+                    final String ip = proxy.split(":")[0];
+                    final String port=proxy.split(":")[1];
+                    /*Thread thread = new Thread(new Runnable() { // Многопоточный способ № 3
                         @Override
                         public void run() {
                             System.out.println(checkProxy(ip,Integer.parseInt(port)));
                         }
                     });
+                    thread.start();*/
+
+                    /*CheckProxy checkProxy = new CheckProxy(ip, Integer.parseInt(port)); // Многопоточный способо № 1
+                    checkProxy.start();
+                    arrayList.add(checkProxy);*/
+
+                    Thread thread = new Thread(new CheckProxy2(ip, Integer.parseInt(port))); // Многопоточнфй способ № 2
                     thread.start();
+                    arrayList2.add(thread);
+
                     proxy ="";
                 }else if(i != 9){
                     proxy += (char)i;
@@ -59,7 +73,7 @@ public class Main {
         }*/
     }
 
-    public static String checkProxy(String ip, int port){
+    public static String checkProxy (String ip, int port){
         try {
             Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip, port));
             URLConnection urlConnection = new URL("https://vozhzhaev.ru/test.php").openConnection(proxy);
@@ -73,6 +87,85 @@ public class Main {
             return "IP: "+ip+":"+port+ANSI_GREEN+" работает"+ANSI_RESET;
         } catch (Exception e) {
             return "IP: "+ip+":"+port+ANSI_RED+" не работает"+ANSI_RESET;
+        }
+    }
+
+}
+
+class CheckProxy extends Thread{
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    private String ip;
+    private int port;
+
+    public CheckProxy(String ip, int port) {
+        super();
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip, port));
+            URLConnection urlConnection = new URL("https://vozhzhaev.ru/test.php").openConnection(proxy);
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(is);
+            int rc;
+            StringBuilder sb = new StringBuilder();
+            while ((rc = reader.read()) != -1){
+                sb.append((char)rc);
+            }
+            System.out.println("IP: "+ip+":"+port+ANSI_GREEN+" работает"+ANSI_RESET);
+        } catch (Exception e) {
+            System.out.println("IP: "+ip+":"+port+ANSI_RED+" не работает"+ANSI_RESET);
+        }
+    }
+}
+
+class CheckProxy2 implements Runnable{
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    private String ip;
+    private int port;
+
+    public CheckProxy2(String ip, int port) {
+        super();
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip, port));
+            URLConnection urlConnection = new URL("https://vozhzhaev.ru/test.php").openConnection(proxy);
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(is);
+            int rc;
+            StringBuilder sb = new StringBuilder();
+            while ((rc = reader.read()) != -1){
+                sb.append((char)rc);
+            }
+            System.out.println("IP: "+ip+":"+port+ANSI_GREEN+" работает"+ANSI_RESET);
+        } catch (Exception e) {
+            System.out.println("IP: "+ip+":"+port+ANSI_RED+" не работает"+ANSI_RESET);
         }
     }
 }
