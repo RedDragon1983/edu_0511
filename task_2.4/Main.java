606a2cbbb0c474c8695612c5fc7c2582dc40eea4
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-23 18:29:52 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -1,7 +1,7 @@
 /*
 Задание: Пользователь вводит сумму вклада и процент, который будет начисляться ежегодно. Отобразить размер вклада поочередно на ближайшие 5 лет.
 */
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
