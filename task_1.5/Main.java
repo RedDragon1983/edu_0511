ff57dd128fdd3c83225cf6358e04b5be6661caed
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-23 18:52:33 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -15,6 +15,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
-        System.out.println((9 / 5) * a + 32);
+        System.out.println((9.0 / 5.0) * a + 32);
     }
 }
