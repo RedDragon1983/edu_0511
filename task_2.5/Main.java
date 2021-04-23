159fd1a166b8fd981667fb8a183012cc806229d2
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-23 18:30:28 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -1,7 +1,7 @@
 /*
 Задание: Запросить у пользователя ввод числа и сохранить это число в переменную a.Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.
 */
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
