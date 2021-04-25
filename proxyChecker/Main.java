1ffb0fddda58acee029d2b82a0d5670a5fe47f2c
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-25 20:15:47 +0300

    Update Main.java

--- proxyChecker/Main.java
+++ proxyChecker/Main.java
@@ -4,7 +4,7 @@ import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-
+import java.io.FileWriter;
 /*
 * task_7.1 - реализовать многопоточность в прокси чеккере первым и вторым способом
 * task_7.2 - рабочие ip адреса необходимо складывать в файл good.txt
@@ -36,21 +36,21 @@ public class Main {
                 else if(i==10){
                     final String ip = proxy.split(":")[0];
                     final String port=proxy.split(":")[1];
-                    /*Thread thread = new Thread(new Runnable() { // Многопоточный способ № 3
+                    Thread thread = new Thread(new Runnable() { // Многопоточный способ № 3
                         @Override
                         public void run() {
                             System.out.println(checkProxy(ip,Integer.parseInt(port)));
                         }
                     });
-                    thread.start();*/
+                    thread.start();
 
                     /*CheckProxy checkProxy = new CheckProxy(ip, Integer.parseInt(port)); // Многопоточный способо № 1
                     checkProxy.start();
                     arrayList.add(checkProxy);*/
 
-                    Thread thread = new Thread(new CheckProxy2(ip, Integer.parseInt(port))); // Многопоточнфй способ № 2
+                    /*Thread thread = new Thread(new CheckProxy2(ip, Integer.parseInt(port))); // Многопоточнфй способ № 2
                     thread.start();
-                    arrayList2.add(thread);
+                    arrayList2.add(thread);*/
 
                     proxy ="";
                 }else if(i != 9){
@@ -84,6 +84,23 @@ public class Main {
             while ((rc = reader.read()) != -1){
                 sb.append((char)rc);
             }
+            
+            // Запись работающих PROXY в файл
+            File file = new File("C:\\java\\work_ip.txt");
+            FileWriter fr = null;
+            try {
+                fr = new FileWriter(file, true);
+                fr.write("IP: "+ip+":"+port+" работает\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                try {
+                    fr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            
             return "IP: "+ip+":"+port+ANSI_GREEN+" работает"+ANSI_RESET;
         } catch (Exception e) {
             return "IP: "+ip+":"+port+ANSI_RED+" не работает"+ANSI_RESET;
