cf722090861cb16d9464138d0fdd3c9c6b7c1153
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-04-29 19:06:34 +0300

    Update Client.java

--- Chat/Client/Client.java
+++ Chat/Client/Client.java
@@ -4,22 +4,22 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Client {
-    private static int id = 0;
+    private static int id = 0; // уникальный номер пользователя
     private static boolean sendName = false;
     public static void main(String[] args) {
-        id = new Random().nextInt(1000000); // уникальный номер ))) пользователя
         try {
-            Socket socket = new Socket("192.168.1.73",8188);
+            Socket socket = new Socket("localhost",8188);
             //Socket socket = new Socket("localhost",8189);
             System.out.println("Подключился");
             final DataInputStream  in =new DataInputStream(socket.getInputStream());
             DataOutputStream out=new DataOutputStream(socket.getOutputStream());
             String response = in.readUTF(); // Ждём сообщение от сервера
-            System.out.println("Ответ от сервера: "+response);
+            String[] partResponse = response.split(" : ");
+            id = Integer.parseInt(partResponse[1]);
+            System.out.println("Ответ от сервера: "+partResponse[0]);
             Scanner scanner = new Scanner(System.in);
             Thread thread = new Thread(new Runnable() {
                 @Override
@@ -37,7 +37,7 @@ public class Client {
             thread.start();
             while(true){
                 String request = scanner.nextLine(); // Читам пользовательский ввод
-                out.writeUTF(id+" : "+request); // Отправляем на сервер id плюс текст с консоли
+                out.writeUTF(id+" : "+request); // Отправляем на сервер id плюс текст с коносли
             }
         } catch (IOException exception) {
             exception.printStackTrace();
